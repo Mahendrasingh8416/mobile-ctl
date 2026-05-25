c1a71388cfce47278d86156d6063fe518268624f
asheeshkundara <asheeshkundara@gmail.com>
2026-05-25 11:58:48 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="container mt-5 text-center">
       <h1 className="text-primary">Bootstrap Working ✅</h1>
       <p className="lead">Next.js + Bootstrap successfully installed</p>
-      <button className="btn btn-success">Click Me</button>
+      <button className="btn btn-success">Click kar na sale</button>
     </div>
   );
 }
