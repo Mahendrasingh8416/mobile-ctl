aae866063cd82b12bbe42325ad4caaabbc5a8dca
Mahendrasingh8416 <smahendra7026@gmail.com>
2026-05-25 17:11:09 +0530

    my second

---
+++ src/app/e-commerce-page/componant/fbanner.js
@@ -0,0 +1,4 @@
+
+<div class="container-fluid" style={{backgroundColor:"yellow",width:"50%",eight:"50%"}}>
+
+</div>
---
+++ src/app/e-commerce-page/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import fbanner from ".componant/fbanner"
+
+const page = () => {
+  return (
+    <><fbanner />
+    <div>page</div>
+    </>
+
+  )
+}
+
+export default page
