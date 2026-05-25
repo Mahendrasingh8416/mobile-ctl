5462152fe9c29ce68608a73c710632a34b0f5c64
Mahendrasingh8416 <smahendra7026@gmail.com>
2026-05-25 14:28:18 +0530

    add

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-export default function Home() {
+import React from 'react'
+
+const page = () => {
   return (
-    <div className="container mt-5 text-center">
-      <h1 className="text-primary">Bootstrap Working ✅</h1>
-      <p className="lead">Next.js + Bootstrap successfully installed</p>
-      <button className="btn btn-success">Click kar na sale</button>
-    </div>
-  );
+    <div>page</div>
+  )
 }
+
+export default page
