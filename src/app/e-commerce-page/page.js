358ce53fd865446f6cd8e2adb5f1f20fbc419a27
Mahendrasingh8416 <smahendra7026@gmail.com>
2026-05-26 10:50:36 +0530

    Update page.js

--- src/app/e-commerce-page/page.js
+++ src/app/e-commerce-page/page.js
@@ -5,30 +5,7 @@ const page = () => {
   return (
     <><fbanner />
     <div>page</div>
-    <h1>heyyy</h1>
-    <h1>heyyy</h1>
     
-    <h1>heyyy</h1>
-    <h1>heyyy</h1>
-    <h1>heyyy</h1>
-     <h1>heyyy</h1>
-    <h1>heyyy</h1>
-    
-    <h1>heyyy</h1>
-    <h1>heyyy</h1>
-    <h1>heyyy</h1>
-     <h1>heyyy</h1>
-    <h1>heyyy</h1>
-    
-    <h1>heyyy</h1>
-    <h1>heyyy</h1>
-    <h1>heyyy</h1>
-     <h1>heyyy</h1>
-    <h1>heyyy</h1>
-    
-    <h1>heyyy</h1>
-    <h1>heyyy</h1>
-    <h1>heyyy</h1>
     </>
 
   )
