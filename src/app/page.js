71bed0ad156bf917e227270cf2dbdf996dcd83af
dhanraj singh <dhanocya@gmail.com>
2026-05-26 14:29:12 +0530

    ok thats it

--- src/app/page.js
+++ src/app/page.js
@@ -1,62 +1,9 @@
 import Header from "@/app/glocompo/Header";
 
-
-// import CategoryStrip from "/components/CategoryStrip";
-
-// import ProcessSection from "/components/ProcessSection";
-// import Services from "/components/Services";
-// import Testimonials from "/components/Testimonials";
-// import CTASection from "/components/CTASection";
-
-import Category from "/componets/Category"
-
-import Footer from "/glocompo/Footer";
-
 export default function Page() {
   return (
   <>
-      <div className="container-fluid p-0 container-outer">
-
-      <div className="container bg-white p-0">
-
-        <Header />
-
-        <main>
-
-          <section className="p-0">
-            <Hero />
-          </section>
-
-          <section>
-            <Category />
-          </section>
-
-          <section>
-            <Blue />
-          </section>
-
-          <section>
-            <ProcessSection />
-          </section>
-
-          <section>
-            <Services />
-          </section>
-
-          <section>
-            <Testimonials />
-          </section>
-
-          <section>
-            <CTASection />
-          </section>
-
-        </main>
-
-        <Footer />
-
-      </div>
-    </div>
+  <Header/>
   </>
   )
 }
