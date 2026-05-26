45f17ea5acaef2642c7c9a9f2901e02d08277125
asheeshkundara <asheeshkundara@gmail.com>
2026-05-26 10:21:25 +0530

    just add hero banner

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,20 +4,30 @@ export default function Hero() {
   return (
    <section className="hero">
     <div className="hero-box">
-     <Image
-      src={"/images/mobilebanner.png"}
-      alt="ok"
-      width={auto
-      
-      />
-      <div  className="row align-items-center">
-            <h1>
+   
+     
+           <div className="img-txt ">
+             <h1>
                 Technology Built for the <br />
                 Way People Actually Work
               </h1>
-      </div>
-
+           </div>
+    
      
+    </div>
+     <div className="para" >
+       <p>
+          We bridge the gap between complex engineering and human intuition to build tech that users actually enjoy using.
+      </p>
+       <div className="hero-btn">
+          <button className="custom-btn me-2">
+            View Our Portfolio
+          </button>
+
+          <button className="custom-btn">
+            Work With Us
+          </button>
+          </div>
     </div>
 
    </section>
