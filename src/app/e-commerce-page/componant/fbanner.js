a89795452ba8741b524fd75d28750a63daa72e26
Mahendrasingh8416 <smahendra7026@gmail.com>
2026-05-26 12:40:48 +0530

    udate without rizn
    ashu

--- src/app/e-commerce-page/componant/fbanner.js
+++ src/app/e-commerce-page/componant/fbanner.js
@@ -27,57 +27,15 @@ export default function Fbanner() {
             <p className="fontmainText text-center text-lg-start mb-4">
               Your e-commerce system shouldn’t slow you down- <br></br> it should move your business forward.
             </p>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            </p>
-            <p className="fontmainText text-center text-lg-start mb-4">
-              Your e-commerce system shouldn’t slow you down- <br></br> it should move your business forward.
-            </p>
-
-            </p>
-            <p className="fontmainText text-center text-lg-start mb-4">
-              Your e-commerce system shouldn’t slow you down- <br></br> it should move your business forward.
-            </p>
-
-            </p>
-            <p className="fontmainText text-center text-lg-start mb-4">
-              Your e-commerce system shouldn’t slow you down- <br></br> it should move your business forward.
-            </p>
-
-
-
-
-
+            
 
             {/* Buttons */}
-            <div className="d-flex flex-column align-items-center flex-lg-row gap-3 mt-4"style={{width:"50%"}}>
+            {/* <div className="d-flex flex-column align-items-center flex-lg-row gap-3 mt-4"style={{width:"50%"}}>
               <button className="btn-warning-custom w-100 max-w-sm"> View Our Portfolio </button>
               <button className="btn-warning-custom w-100 max-w-sm"> Work With Us </button>
             </div>
           </div>
-
+ */}
 
           {/* Right Column: Desktop Background Spacer */}
           <div className="col-lg-5 d-none d-lg-block">
